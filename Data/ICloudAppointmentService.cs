1e51883fb03840ed24b9bb7169e51b5243fe1522
Bogdan Mezei <bogdan335@hotmail.com>
2020-12-07 15:36:17 +0100

    Backup

---
+++ Data/ICloudAppointmentService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SEP3.Model;
+
+namespace SEP3.Data
+{
+    public interface ICloudAppointmentService
+    {
+        public Task<List<Appointment>> GetAllForPatient(int id);
+        public Task<List<Appointment>> GetAllForDoctor(int id);
+    }
+}
---
+++ Data/ICloudUserService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SEP3.Model;
+
+namespace SEP3.Data
+{
+    public interface ICloudUserService
+    {
+        public Task<LoggedUser> ValidateUser(string idNr, string password);
+        public Task ValidateManager(int idNr);
+        public Task AddUser(User user);
+        public Task AddDoctor(User user);
+        public Task<User> GetUser(string idNr);
+        public Task<List<User>> GetUnvalidatedUsers();
+        public Task EditUser(User user);
+    }
+}
